48ca0c865c9cec1dd6e880cfc7fb418dfa665a13
Jeff W <Jeff@fraction.rent>
2020-03-31 04:09:13 -0400

    Updates

---
+++ precache-manifest.27c37d9dd25bb0763e9a87bf21b82b61.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eded1b4003d94c524b29e03cfb589080",
+    "url": "/watch-rap-tv/index.html"
+  },
+  {
+    "revision": "e37f92a05ee29e31e9d2",
+    "url": "/watch-rap-tv/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "315ee987864dadf1e801",
+    "url": "/watch-rap-tv/static/js/2.688b8110.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/watch-rap-tv/static/js/2.688b8110.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e37f92a05ee29e31e9d2",
+    "url": "/watch-rap-tv/static/js/main.bd40316b.chunk.js"
+  },
+  {
+    "revision": "c5ba93d2650c383c571b",
+    "url": "/watch-rap-tv/static/js/runtime-main.c727ce4b.js"
+  }
+]);
